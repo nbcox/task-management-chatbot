1feb4e3363e238091b5fdb1ee9dc781281218dc8
Nick Cox <nbcox8@outlook.com>
2023-12-10 16:01:37 -0600

    Added ability to inquire about tasks, more error handling

--- task_chatbot.py
+++ task_chatbot.py
@@ -10,7 +10,7 @@ class TaskChatbot:
     def start_chat(self):
         print("Welcome to the Task Chatbot!")
         print(
-            "You can create, assign, mark as complete, delete, and discuss tasks. Type 'exit' to end the chat.\n"
+            "You can create, assign, mark as complete, delete, inquire about tasks, and discuss tasks. Type 'exit' to end the chat.\n"
         )
 
         while True:
@@ -23,6 +23,7 @@ class TaskChatbot:
 
     def process_user_input(self, user_input):
         user_input = user_input.lower()
+
         # Basic command recognition for demonstration
         if "create task" in user_input:
             task_name = user_input.split("create task ")[-1]
@@ -33,11 +34,21 @@ class TaskChatbot:
             )
             self.assign_task(task_index, assignee)
         elif "mark as complete" in user_input:
-            task_index = int(user_input.split("mark as complete ")[-1]) - 1
+            try:
+                task_index = int(user_input.split("mark as complete ")[-1]) - 1
+            except ValueError:
+                print("Chatbot: Invalid task index. Please enter a valid number.")
+                return
             self.mark_task_complete(task_index)
         elif "delete task" in user_input:
-            task_index = int(user_input.split("delete task ")[-1]) - 1
+            try:
+                task_index = int(user_input.split("delete task ")[-1]) - 1
+            except ValueError:
+                print("Chatbot: Invalid task index. Please enter a valid number.")
+                return
             self.delete_task(task_index)
+        elif "inquire about tasks" in user_input:
+            self.inquire_about_tasks()
         elif "show tasks" in user_input:
             self.show_tasks()
         else:
@@ -51,7 +62,12 @@ class TaskChatbot:
         print(f"Chatbot: Task '{task_name}' created successfully!")
 
     def assign_task(self, task_index, assignee):
-        task_index = int(task_index) - 1
+        try:
+            task_index = int(task_index) - 1
+        except ValueError:
+            print("Chatbot: Invalid task index. Please enter a valid number.")
+            return
+
         if 0 <= task_index < len(self.tasks):
             self.tasks[task_index]["assignee"] = assignee
             print(
@@ -81,6 +97,32 @@ class TaskChatbot:
             print(
                 "Chatbot: Invalid task index. Please check the task index and try again."
             )
+
+    def inquire_about_tasks(self):
+        if not self.tasks:
+            print("Chatbot: No tasks available.")
+        else:
+            print(
+                "Chatbot: You can inquire about a specific task by asking about its details, deadline, or progress."
+            )
+            inquiry = input("You: ")
+            task_found = False
+
+            for task in self.tasks:
+                if task["name"].lower() in inquiry.lower():
+                    task_found = True
+                    self.provide_task_details(task)
+                    break
+
+            if not task_found:
+                print(
+                    "Chatbot: Task not found. Please check the task name and try again."
+                )
+
+    def provide_task_details(self, task):
+        print(
+            f"Chatbot: Task Details - Name: {task['name']} | Status: {task['status']} | Assignee: {task.get('assignee', 'Unassigned')}"
+        )
 
     def show_tasks(self):
         if not self.tasks:
